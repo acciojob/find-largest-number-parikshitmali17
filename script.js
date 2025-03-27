f0d6dcc9dcef5cdfee7fff3aa0ec445efd5efa21
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-27 19:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function findLargest(a, b, c) {
   //your code here
+	let largestNum=-Infinity;
+	if(a>b && b>c){
+		largestNum=a;
+	}else if(b>a && a>c){
+		largestNum=b
+	}else if(c>b && b>a){
+		largestNum=c
+	}
+
+	return largestNum
+
+	
+	 
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
